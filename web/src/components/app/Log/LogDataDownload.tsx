66c12e53a4a0811a33e71d753f9a87e55910e4ce
Chris <chrisstudio1016@gmail.com>
2025-05-27 21:10:02 +0800

    handle download csv

--- web/src/components/app/Log/LogDataDownload.tsx
+++ web/src/components/app/Log/LogDataDownload.tsx
@@ -1,22 +1,35 @@
 import { Button } from '@/components/ui/button';
 import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
 
-export interface LogDataDownloadMenuProps<T> {
+export interface LogDataDownloadMenuProps<T extends object> {
     tableRows: T[];
 }
 
-export const LogDataDownloadMenu = <T,>({ tableRows }: LogDataDownloadMenuProps<T>) => {
+export const LogDataDownloadMenu = <T extends object>({ tableRows }: LogDataDownloadMenuProps<T>) => {
     const handleLogDataDownload = (fileType: 'csv' | 'json') => {
         let fileName = '';
+        let data = '';
         switch (fileType) {
-            case 'csv':
+            case 'csv': {
                 fileName = 'data.csv';
+                const headers = Object.keys(tableRows[0]);
+                const rows = tableRows.map((row: T) => {
+                    const stringifiedData = Object.values(row).map((data) => {
+                        if (typeof data === 'object') {
+                            return JSON.stringify(data);
+                        }
+                        return data;
+                    });
+                    return stringifiedData.join(',');
+                });
+                data = [headers.join(','), ...rows].join('\n');
                 break;
+            }
             case 'json':
                 fileName = 'data.json';
+                data = JSON.stringify(tableRows, null, 2);
                 break;
         }
-        const data = JSON.stringify(tableRows, null, 2);
         const blob = new Blob([data], { type: fileType === 'json' ? 'application/json' : 'text/csv' });
         const url = URL.createObjectURL(blob);
         const link = document.createElement('a');
