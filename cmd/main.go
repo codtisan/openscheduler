6924923c00f6e0842c4686feaa8174aa9278214d
Chris <chrisstudio1016@gmail.com>
2025-06-01 00:52:49 +0800

    Init logger

--- cmd/main.go
+++ cmd/main.go
@@ -5,6 +5,7 @@ import (
 	"open-scheduler/api/middleware"
 	"open-scheduler/internal/config"
 	"open-scheduler/pkg/databases"
+	"open-scheduler/pkg/logger"
 
 	"github.com/gofiber/fiber/v3"
 )
@@ -13,6 +14,7 @@ func main() {
 	config.LoadConfig()
 	config.InitValidator()
 	databases.InitMongo()
+	logger.Init()
 	app := fiber.New()
 	middleware.NewMiddlewares(app)
 	api.NewRoutes(app)
