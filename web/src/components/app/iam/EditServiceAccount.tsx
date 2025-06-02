63c0e479bf74ef371f4fd6e5eb557ac56d42cb53
Chris Chan <chrischan@SlimMY.antelope.local>
2025-06-02 13:21:17 +0800

    edit user service account select role

--- web/src/components/app/iam/EditServiceAccount.tsx
+++ web/src/components/app/iam/EditServiceAccount.tsx
@@ -2,6 +2,10 @@ import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import type { IServiceAccountData } from '@/interfaces/service-acount-table';
 import type { Row } from '@tanstack/react-table';
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
+import { Button } from '@/components/ui/button';
+import { RoleDataSample } from '@/constants/role';
+import type { IRoleData } from '@/interfaces/role-table';
 
 export interface EditServiceAccountProps {
     rowData: Row<IServiceAccountData>;
@@ -19,8 +23,21 @@ export const EditServiceAccountSection = ({ rowData }: EditServiceAccountProps) 
                 <Input value={rowData.getValue('username')} />
             </div>
             <div className="flex flex-row gap-5 max-w-[80%] items-center">
-                <Label>Role</Label>
-                <Input value={rowData.getValue('role')} />
+                <Label htmlFor="role" className="text-right">
+                    Role
+                </Label>
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button variant="outline" className="w-50">
+                            Select a role
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent className="w-50">
+                        {RoleDataSample.map((roleData: IRoleData) => {
+                            return <DropdownMenuItem>{roleData.name}</DropdownMenuItem>;
+                        })}
+                    </DropdownMenuContent>
+                </DropdownMenu>
             </div>
         </div>
     );
--- web/src/components/app/iam/EditUser.tsx
+++ web/src/components/app/iam/EditUser.tsx
@@ -2,6 +2,10 @@ import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import type { IUserData } from '@/interfaces/user-table';
 import type { Row } from '@tanstack/react-table';
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
+import { Button } from '@/components/ui/button';
+import { RoleDataSample } from '@/constants/role';
+import type { IRoleData } from '@/interfaces/role-table';
 
 export interface EditUserProps {
     rowData: Row<IUserData>;
@@ -19,8 +23,21 @@ export const EditUserSection = ({ rowData }: EditUserProps) => {
                 <Input value={rowData.getValue('username')} />
             </div>
             <div className="flex flex-row gap-5 max-w-[80%] items-center">
-                <Label>Role</Label>
-                <Input value={rowData.getValue('role')} />
+                <Label htmlFor="role" className="text-right">
+                    Role
+                </Label>
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button variant="outline" className="w-50">
+                            Select a role
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent className="w-50">
+                        {RoleDataSample.map((roleData: IRoleData) => {
+                            return <DropdownMenuItem>{roleData.name}</DropdownMenuItem>;
+                        })}
+                    </DropdownMenuContent>
+                </DropdownMenu>
             </div>
         </div>
     );
