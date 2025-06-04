1d37b9563454df5786c7085e9caf321b7978d389
Chris Chan <chrischan@SlimMY.antelope.local>
2025-06-04 13:19:44 +0800

    add list api in router

--- api/router.go
+++ api/router.go
@@ -9,21 +9,27 @@ import (
 func NewRoutes(app *fiber.App) {
 	app.Get("/", controller.HealthCheckAPI)
 	app.Get("/health", controller.HealthCheckDBAPI)
+
+	app.Get("/user", controller.GetUserListAPI)
 	app.Post("/user/login", controller.UserLoginAPI)
 	app.Post("/user/create", controller.UserCreateAPI)
 	app.Delete("/user/:user_id", controller.UserDeleteAPI)
 	app.Put("/user/:user_id", controller.UserUpdateAPI)
 
+	app.Get("/serviceaccount", controller.GetServiceAccountListAPI)
 	app.Post("/serviceaccount/create", controller.ServiceAccountCreateAPI)
 	app.Delete("/serviceaccount/:account_id", controller.ServiceAccountDeleteAPI)
 	app.Put("/serviceaccount/:account_id", controller.ServiceAccountDeleteAPI)
 
+	app.Get("/role", controller.GetRoleListAPI)
 	app.Post("/role/create", controller.RoleCreateAPI)
-	app.Delete("role/:role_id", controller.RoleDeleteAPI)
+	app.Delete("/role/:role_id", controller.RoleDeleteAPI)
 
-	app.Post("project/create", controller.ProjectCreateAPI)
-	app.Delete("project/:project_id", controller.ProjectDeleteAPI)
-	app.Get("project", controller.GetProjectListAPI)
+	app.Post("/project/create", controller.ProjectCreateAPI)
+	app.Delete("/project/:project_id", controller.ProjectDeleteAPI)
+	app.Get("/project", controller.GetProjectListAPI)
 
 	app.Get("/auditlog", controller.GetAuditLogListAPI)
+	app.Get("/metricslog", controller.GetAuditLogListAPI)
+	app.Get("/responselog", controller.GetAuditLogListAPI)
 }
